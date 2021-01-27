06f7c36bf753b3a903902e685f6e2e2d144f6bc7
grac3gao <gracegao@google.com>
2021-01-26 17:30:04 -0800

    testing

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	cloud.google.com/go/logging v1.0.1-0.20200331222814-69e77e66e597
 	cloud.google.com/go/pubsub v1.8.0
 	cloud.google.com/go/storage v1.10.0
+	github.com/GoogleCloudPlatform/cloudsql-proxy v1.19.1
 	github.com/cloudevents/sdk-go/protocol/pubsub/v2 v2.2.1-0.20200806165906-9ae0708e27fa
 	github.com/cloudevents/sdk-go/v2 v2.3.1
 	github.com/fsnotify/fsnotify v1.4.9
--- test/test_images/sender/main.go
+++ test/test_images/sender/main.go
@@ -17,121 +17,147 @@ limitations under the License.
 package main
 
 import (
-	"context"
-	"encoding/json"
+	//"database/sql"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"time"
 
-	cev2 "github.com/cloudevents/sdk-go/v2"
-	"github.com/cloudevents/sdk-go/v2/protocol"
-	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
+	//"time"
+	//
+	//"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
 	"github.com/kelseyhightower/envconfig"
-	"go.opencensus.io/trace"
-	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/util/retry"
-
-	"github.com/google/knative-gcp/pkg/kncloudevents"
-	"github.com/google/knative-gcp/test/lib"
 )
 
 type envConfig struct {
-	BrokerURLEnvVar string `envconfig:"BROKER_URL" required:"true"`
-	RetryEnvVar     string `envconfig:"RETRY"`
-}
-
-// defaultRetry represents that there will be 4 iterations.
-// The duration starts from 30s and is multiplied by factor 2.0 for each iteration.
-var defaultRetry = wait.Backoff{
-	Steps:    4,
-	Duration: 30 * time.Second,
-	Factor:   2.0,
-	// The sleep at each iteration is the duration plus an additional
-	// amount chosen uniformly at random from the interval between 0 and jitter*duration.
-	Jitter: 1.0,
+	DBName string `envconfig:"DB_NAME" required:"true"`
+	DBUser string `envconfig:"DB_USER" required:"true"`
 }
 
 func main() {
-
 	var env envConfig
 	if err := envconfig.Process("", &env); err != nil {
 		panic(fmt.Sprintf("Failed to process env var: %s", err))
 	}
 
-	brokerURL := env.BrokerURLEnvVar
-	needRetry := (env.RetryEnvVar == "true")
+	//panic(env.dbName)
 
-	ceClient, err := kncloudevents.NewDefaultClient(brokerURL)
+	cfg := mysql.Cfg("gracegao-knative-testing:us-central1:sqlinstance-mysql", env.DBUser, "")
+	cfg.DBName = env.DBName
+	cfg.ParseTime = true
+
+	const timeout = 10 * time.Second
+	cfg.Timeout = timeout
+	cfg.ReadTimeout = timeout
+	cfg.WriteTimeout = timeout
+
+	db, err := mysql.DialCfg(cfg)
 	if err != nil {
-		fmt.Printf("Unable to create ceClient: %s ", err)
+		panic("couldn't dial: " + err.Error())
 	}
-
-	// If needRetry is true, repeat sending Event with exponential backoff when there are some specific errors.
-	// In e2e test, sync problems could cause 404 and 5XX error, retrying those would help reduce flakiness.
-	success := true
-	span, err := sendEvent(ceClient, needRetry)
-	if !cev2.IsACK(err) {
-		success = false
-		fmt.Printf("failed to send event: %v", err)
-	}
-
-	if err := writeTerminationMessage(map[string]interface{}{
-		"success": success,
-		"traceid": span.SpanContext().TraceID.String(),
-	}); err != nil {
-		fmt.Printf("failed to write termination message, %s.\n", err)
-	}
+	defer db.Close()
+	var now time.Time
+	fmt.Println(db.QueryRow("SELECT NOW()").Scan(&now))
+	fmt.Println(db.Stats())
+	fmt.Println(now)
+	//_, err := sql.Open("mysql", env.dbUser + ":@/" + env.dbName)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer db.Close()
+	//fmt.Sprintf("%v", db.Stats())
 }
 
-func sendEvent(ceClient cev2.Client, needRetry bool) (span *trace.Span, err error) {
-	send := func() error {
-		ctx := cev2.WithEncodingBinary(context.Background())
-		ctx, span = trace.StartSpan(ctx, "sender", trace.WithSampler(trace.AlwaysSample()))
-		defer span.End()
-		result := ceClient.Send(ctx, sampleCloudEvent())
-		return result
-	}
-
-	if needRetry {
-		err = retry.OnError(defaultRetry, isRetryable, send)
-	} else {
-		err = send()
-	}
-	return
-}
-
-func sampleCloudEvent() cev2.Event {
-	event := cev2.NewEvent(cev2.VersionV1)
-	event.SetID(lib.E2ESampleEventID)
-	event.SetType(lib.E2ESampleEventType)
-	event.SetSource(lib.E2ESampleEventSource)
-	event.SetData(cev2.ApplicationJSON, `{"source": "sender!"}`)
-	return event
-}
-
-func writeTerminationMessage(result interface{}) error {
-	b, err := json.Marshal(result)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile("/dev/termination-log", b, 0644)
-}
-
-// isRetryable determines if the err is an error which is retryable
-func isRetryable(err error) bool {
-	var result *cehttp.Result
-	if protocol.ResultAs(err, &result) {
-		// Potentially retry when:
-		// - 404 Not Found
-		// - 500 Internal Server Error, it is currently for reducing flakiness caused by Workload Identity credential sync up.
-		// We should remove it after https://github.com/google/knative-gcp/issues/1058 lands, as 500 error may indicate bugs in our code.
-		// - 503 Service Unavailable (with or without Retry-After) (IGNORE Retry-After)
-		sc := result.StatusCode
-		if sc == 404 || sc == 500 || sc == 503 {
-			log.Printf("got error: %s, retry sending event. \n", result.Error())
-			return true
-		}
-	}
-	return false
-}
+//// defaultRetry represents that there will be 4 iterations.
+//// The duration starts from 30s and is multiplied by factor 2.0 for each iteration.
+//var defaultRetry = wait.Backoff{
+//	Steps:    4,
+//	Duration: 30 * time.Second,
+//	Factor:   2.0,
+//	// The sleep at each iteration is the duration plus an additional
+//	// amount chosen uniformly at random from the interval between 0 and jitter*duration.
+//	Jitter: 1.0,
+//}
+//
+//func main() {
+//
+//	var env envConfig
+//	if err := envconfig.Process("", &env); err != nil {
+//		panic(fmt.Sprintf("Failed to process env var: %s", err))
+//	}
+//
+//	brokerURL := env.BrokerURLEnvVar
+//	needRetry := (env.RetryEnvVar == "true")
+//
+//	ceClient, err := kncloudevents.NewDefaultClient(brokerURL)
+//	if err != nil {
+//		fmt.Printf("Unable to create ceClient: %s ", err)
+//	}
+//
+//	// If needRetry is true, repeat sending Event with exponential backoff when there are some specific errors.
+//	// In e2e test, sync problems could cause 404 and 5XX error, retrying those would help reduce flakiness.
+//	success := true
+//	span, err := sendEvent(ceClient, needRetry)
+//	if !cev2.IsACK(err) {
+//		success = false
+//		fmt.Printf("failed to send event: %v", err)
+//	}
+//
+//	if err := writeTerminationMessage(map[string]interface{}{
+//		"success": success,
+//		"traceid": span.SpanContext().TraceID.String(),
+//	}); err != nil {
+//		fmt.Printf("failed to write termination message, %s.\n", err)
+//	}
+//}
+//
+//func sendEvent(ceClient cev2.Client, needRetry bool) (span *trace.Span, err error) {
+//	send := func() error {
+//		ctx := cev2.WithEncodingBinary(context.Background())
+//		ctx, span = trace.StartSpan(ctx, "sender", trace.WithSampler(trace.AlwaysSample()))
+//		defer span.End()
+//		result := ceClient.Send(ctx, sampleCloudEvent())
+//		return result
+//	}
+//
+//	if needRetry {
+//		err = retry.OnError(defaultRetry, isRetryable, send)
+//	} else {
+//		err = send()
+//	}
+//	return
+//}
+//
+//func sampleCloudEvent() cev2.Event {
+//	event := cev2.NewEvent(cev2.VersionV1)
+//	event.SetID(lib.E2ESampleEventID)
+//	event.SetType(lib.E2ESampleEventType)
+//	event.SetSource(lib.E2ESampleEventSource)
+//	event.SetData(cev2.ApplicationJSON, `{"source": "sender!"}`)
+//	return event
+//}
+//
+//func writeTerminationMessage(result interface{}) error {
+//	b, err := json.Marshal(result)
+//	if err != nil {
+//		return err
+//	}
+//	return ioutil.WriteFile("/dev/termination-log", b, 0644)
+//}
+//
+//// isRetryable determines if the err is an error which is retryable
+//func isRetryable(err error) bool {
+//	var result *cehttp.Result
+//	if protocol.ResultAs(err, &result) {
+//		// Potentially retry when:
+//		// - 404 Not Found
+//		// - 500 Internal Server Error, it is currently for reducing flakiness caused by Workload Identity credential sync up.
+//		// We should remove it after https://github.com/google/knative-gcp/issues/1058 lands, as 500 error may indicate bugs in our code.
+//		// - 503 Service Unavailable (with or without Retry-After) (IGNORE Retry-After)
+//		sc := result.StatusCode
+//		if sc == 404 || sc == 500 || sc == 503 {
+//			log.Printf("got error: %s, retry sending event. \n", result.Error())
+//			return true
+//		}
+//	}
+//	return false
+//}
